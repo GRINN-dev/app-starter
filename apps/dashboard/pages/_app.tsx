bf9aee9d56cce9c121f768af978ff53542711e97
bat <baptiste.monet@gmail.com>
2022-06-21 13:51:28 +0200

    reinit from base branch + add google sign-in ofr test

--- apps/dashboard/pages/_app.tsx
+++ apps/dashboard/pages/_app.tsx
@@ -5,7 +5,7 @@ import "@grinn/styles/dist/output.css";
 import { ApolloProviderWrapper } from "@grinn/lib";
 
 import "react-circular-progressbar/dist/styles.css";
-
+import Script from "next/script";
 function MyApp({ Component, pageProps }) {
   return (
     <>
@@ -17,7 +17,14 @@ function MyApp({ Component, pageProps }) {
         />
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width" />
+
+        <meta
+          name="google-signin-client_id"
+          content="128517360182-uqnaqd02h6ab6f65uragsmh0j6no516q.apps.googleusercontent.com"
+        />
       </Head>
+      <Script src="https://apis.google.com/js/platform.js"></Script>
+
       <ApolloProviderWrapper
         endpoint={process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT}
       >
--- apps/dashboard/pages/index.tsx
+++ apps/dashboard/pages/index.tsx
@@ -1,7 +1,29 @@
 import { NextPage } from "next";
+import {
+  GoogleLogin,
+  GoogleLoginResponse,
+  GoogleLoginResponseOffline,
+} from "react-google-login";
 
 const Home: NextPage = () => {
-  return <div>Hello world</div>;
+  const responseGoogle = (
+    response: GoogleLoginResponse | GoogleLoginResponseOffline
+  ) => {
+    console.log("res: ", response);
+  };
+  return (
+    <div>
+      <div className="g-signin2" data-onsuccess="responseGoogle"></div>
+      {/* <GoogleLogin
+        clientId="128517360182-uqnaqd02h6ab6f65uragsmh0j6no516q.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={"single_host_origin"}
+        redirectUri="http://localhost:3000/auth"
+      /> */}
+    </div>
+  );
 };
 
 export default Home;
