f639c4ca1c5d32905dc271c610a1266432d0cf06
ThibautdelaChesnais <thibaut.c@obole.eu>
2022-07-07 10:02:01 +0200

    auth fonctionneelle

--- apps/dashboard/pages/auth.tsx
+++ apps/dashboard/pages/auth.tsx
@@ -2,18 +2,44 @@ import { NextPage } from "next";
 import { useRouter } from "next/router";
 import { LoginForm, LoginFormData } from "@grinn/components";
 import {
+  GetCurrentUserQuery,
   useAuthenticateMutation,
+  useGetAllUsersQuery,
   useGetCurrentUserLazyQuery,
   useGetCurrentUserQuery,
 } from "@grinn/graphql-generated";
-import { useContext } from "react";
+import {
+  createContext,
+  Dispatch,
+  SetStateAction,
+  useContext,
+  useEffect,
+} from "react";
 import { TokenContext } from "@grinn/lib";
+
+export const UserContext = createContext<{
+  user: GetCurrentUserQuery;
+  setUser: Dispatch<SetStateAction<any>>;
+}>({ user: null, setUser: () => {} });
 
 const AuthPage: NextPage = () => {
   const router = useRouter();
   const [authenticate] = useAuthenticateMutation();
-  const [loadCurrentUser] = useGetCurrentUserLazyQuery({});
+  const [loadCurrentUser, { data: dataUser }] = useGetCurrentUserLazyQuery({});
   const { accessToken, setAccessToken } = useContext(TokenContext);
+  const { user, setUser } = useContext(UserContext);
+
+  useEffect(() => {
+    if (dataUser?.currentUser) {
+      setUser(dataUser);
+      !dataUser?.currentUser?.isAdmin
+        ? router.replace("/dashboard")
+        : router.replace("/dashboard-admin");
+    }
+  }, [dataUser]);
+  useEffect(() => {
+    loadCurrentUser({ fetchPolicy: "network-only" });
+  }, [accessToken]);
   return (
     <div className="flex justify-center">
       <LoginForm
@@ -22,19 +48,25 @@ const AuthPage: NextPage = () => {
             variables: { input: data },
           });
           const { access_token } = res.authenticate;
-          console.log(
-            "🚀 ~ file: auth.tsx ~ line 23 ~ onSubmit={ ~ jwt",
-            access_token
-          );
           setAccessToken(access_token);
-
-          loadCurrentUser({
+          /*
+          const test = await loadCurrentUser({
             fetchPolicy: "network-only",
-          }).then(data => {
-            !data?.data?.currentUser?.isAdmin
-              ? router.replace("/dashboard")
-              : router.replace("/dashboard-admin");
-          });
+          })
+            .then(userData => {
+              console.log("eibifr ");
+              !userData?.data?.currentUser?.isAdmin
+                ? router.replace("/dashboard")
+                : router.replace("/dashboard-admin");
+            })
+            .catch(err => {
+              console.log(err);
+            })
+            .finally(() => {
+              console.log("finally");
+            });
+          console.log("test : ", test);
+          */
         }}
       />
     </div>
